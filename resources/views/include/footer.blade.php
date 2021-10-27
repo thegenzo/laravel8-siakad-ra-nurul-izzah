a2e544906e123340dc954be3883b62fffa737863
Dicky Agusputra <loscarbulos@gmail.com>
2021-10-27 20:01:56 +0800

    minor fix

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
   <div class="footer-left">
-    Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+    Copyright &copy; 2021 <div class="bullet"></div> Universitas Dayanu Ikhsanuddin
   </div>
   <div class="footer-right">
-    2.3.0
+    Teknik Informatika
   </div>
 </footer>
--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -25,6 +25,12 @@
           </ul>
         </li>
         <li class="{{ Request::is('pengumuman') ? ' active' : '' }}"><a class="nav-link" href="/pengumuman"><i class="fas fa-pencil-ruler"></i> <span>Pengumuman</span></a></li>
+        <li class="nav-item dropdown {{ Request::is('nilai') || Request::is('sikap') || Request::is('rapor') ? ' active' : '' }}">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Penilaian</span></a>
+          <ul class="dropdown-menu">
+            <li class="{{ Request::is('nilai') ? ' active' : '' }}"><a class="nav-link" href="{{ route('nilai.index') }}">Nilai</a></li>
+          </ul>
+        </li>
         <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
         <li class="nav-item dropdown">
           <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Bootstrap</span></a>
