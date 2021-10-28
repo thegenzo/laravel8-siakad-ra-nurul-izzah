c54ae8bd427bc95d08b8de8eb48aa1a2ed6cc8fb
Dicky Agusputra <loscarbulos@gmail.com>
2021-10-28 14:01:34 +0800

    fitur homepage

---
+++ resources/views/pages/homepage/include/navbar.blade.php
@@ -0,0 +1,23 @@
+<div class="navbar-bg"></div>
+<nav class="navbar navbar-expand-lg main-navbar">
+  <a href="/" class="navbar-brand sidebar-gone-hide">SIAKAD RA Nurul Izzah</a>
+  <div class="navbar-nav">
+    <a href="#" class="nav-link sidebar-gone-show" data-toggle="sidebar"><i class="fas fa-bars"></i></a>
+  </div>
+  <div class="nav-collapse">
+    <a class="sidebar-gone-show nav-collapse-toggle nav-link" href="#">
+      <i class="fas fa-ellipsis-v"></i>
+    </a>
+    {{-- <ul class="navbar-nav">
+      <li class="nav-item active"><a href="#" class="nav-link">Application</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Report Something</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Server Status</a></li>
+    </ul> --}}
+  </div>
+  <form class="form-inline ml-auto">
+    
+  </form>
+  <ul class="navbar-nav navbar-right">
+    <li class="nav-item"><a href="/login" class="nav-link">Login</a></li>
+  </ul>
+</nav>
