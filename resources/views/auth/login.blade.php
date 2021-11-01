55978f66829bfc2693cea6a6a2c9a6ea8af4e780
Dicky Agusputra <loscarbulos@gmail.com>
2021-11-01 12:29:50 +0800

    minor fix
    menambahkan anchor pada logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,8 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="{{ asset('/assets/img/logo.jpg') }}" alt="logo" width="100" class="shadow-light rounded-circle">
+              <a href="/"><img src="{{ asset('/assets/img/logo.jpg') }}" alt="logo" width="100" class="shadow-light rounded-circle">  </a>
+              
             </div>
 
             <div class="card card-primary">
